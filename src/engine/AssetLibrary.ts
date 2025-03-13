340bfc6e03a8a840bdb1f5324241c7bfcf631862
Alexis BRIARD <briard.alexis@hotmail.fr>
2025-03-13 23:04:03 +0100

    github workflow

--- src/engine/AssetLibrary.ts
+++ src/engine/AssetLibrary.ts
@@ -1,144 +1,139 @@
 const assets: Record<string, HTMLImageElement> = {};
 
 export const init = async () => {
-  await loadAsset("player", "troll.png");
-  await loadAsset("bouclier", "bouclier.png");
-  await loadAsset("chicken", "chicken.png");
-  await loadAsset("anneau", "anneau.png");
-  await loadAsset("appareil", "appareil.png");
-  await loadAsset("arbre", "arbre1.png");
-  await loadAsset("archer", "archer.png");
-  await loadAsset("armure", "armure.png");
-  await loadAsset("backpack", "backpack.png");
-  await loadAsset("bibliotheque", "bibliotheque.png");
-  await loadAsset("bouclier", "bouclier.png");
-  await loadAsset("carte-au-tresor", "carte-au-tresor.png");
-  await loadAsset("carte-au-tresor-bis", "carte-au-tresor-bis.png");
-  await loadAsset("chandelier", "chandelier.png");
-  await loadAsset("chicken", "chicken.png");
-  await loadAsset("chevre01", "chevre01.png");
-  await loadAsset("chevre02", "chevre02.png");
-  await loadAsset("cle", "cle.png");
-  await loadAsset("cleBlue", "cleBlue.png");
-  await loadAsset("cleGreen", "cleGreen.png");
-  await loadAsset("cleRed", "cleRed.png");
-  await loadAsset("collier", "collier.png");
-  await loadAsset("communsword", "communsword.png");
-  await loadAsset("cor-des-alpes", "cor-des-alpes.png");
-  await loadAsset("diamantRouge", "diamantRouge.png");
-  await loadAsset("diamantBleu", "diamantBleu.png");
-  await loadAsset("doorleft", "doorleft.png");
-  await loadAsset("doorright", "doorright.png");
-  await loadAsset("dragon", "dragon.png");
-  await loadAsset("dwarf", "dwarf.png");
-  await loadAsset("elf", "elf.png");
-  await loadAsset("fond-carte", "fond-carte.jpg");
-  await loadAsset("forgeron", "forgeron.png");
-  await loadAsset("frankenstein", "frankenstein.png");
-  await loadAsset("gateaux", "gateaux.png");
-  await loadAsset("gem", "gem.png");
-  await loadAsset("group", "group.png");
-  await loadAsset("guillotine", "guillotine.png");
-  await loadAsset("hache", "hache.png");
-  await loadAsset("king", "king.png");
-  await loadAsset("knight", "knight.png");
-  await loadAsset("la-tombe", "la-tombe.png");
-  await loadAsset("levier", "levier.png");
-  await loadAsset("magicsword", "magicsword.png");
-  await loadAsset("mission", "mission.png");
-  await loadAsset("necromancer", "necromancer.png");
-  await loadAsset("ogre01", "ogre01.png");
-  await loadAsset("ogre02", "ogre02.png");
-  await loadAsset("ogre03", "ogre03.png");
-  await loadAsset("ogre04", "ogre04.png");
-  await loadAsset("oldchandelier", "oldchandelier.png");
-  await loadAsset("panneau", "panneau.png");
-  await loadAsset("piege", "piege.png");
-  await loadAsset("player", "player.png");
-  await loadAsset("porte", "porte.png");
-  await loadAsset("potion", "potion.png");
-  await loadAsset("potion-bis", "potion-bis.png");
-  await loadAsset("potion-tr", "potion-tr.png");
-  await loadAsset("prison", "prison.png");
-  await loadAsset("prisonBlue", "prisonBlue.png");
-  await loadAsset("prisonGreen", "prisonGreen.png");
-  await loadAsset("prisonRed", "prisonRed.png");
-  await loadAsset("prisonYellow", "prisonYellow.png");
-  await loadAsset("quest", "quest.png");
-  await loadAsset("quete", "quete.png");
-  await loadAsset("raresword", "raresword.png");
-  await loadAsset("role-playing", "role-playing.png");
-  await loadAsset("serpent", "serpent.png");
-  await loadAsset("shaman", "shaman.png");
-  await loadAsset("shield", "shield.png");
-  await loadAsset("squelette", "squelette.png");
-  await loadAsset("table", "table.png");
-  await loadAsset("tasse-a-cafe", "tasse-a-cafe.png");
-  await loadAsset("tonneau", "tonneau.png");
-  await loadAsset("tonneau-de-vin", "tonneau-de-vin.png");
-  await loadAsset("troll", "troll.png");
-  await loadAsset("uniquesword", "uniquesword.png");
-  await loadAsset("village", "village.png");
-  await loadAsset("vortex", "vortex.png");
-  await loadAsset("wizard", "wizard.png");
-  await loadAsset("bg-carrelage", "bg/carrelage.png");
-  await loadAsset("bg-lava", "bg/lave.png");
-  await loadAsset("bg-mur", "bg/mur-donjon.png");
-  await loadAsset("bg-sol", "bg/sol-interieur.png");
-  await loadAsset("bg-water", "bg/eau.png");
-  await loadAsset("bg-herbe", "bg/herbe.png");
-  await loadAsset("bg-chemin", "bg/chemin.png");
-  await loadAsset("echelle", "echelle.png");
-  await loadAsset("trapp", "trapp.png");
-  //
-  await loadAsset("boat01", "boat01.png");
-  await loadAsset("boat02", "boat02.png");
-  await loadAsset("boat03", "boat03.png");
-
-  await loadAsset("tapisMilieu", "dungeon/tapisMilieu.png");
-  await loadAsset("tapisCoteHaut", "dungeon/tapisCoteHaut.png");
-  await loadAsset("tapisCoteBas", "dungeon/tapisCoteBas.png");
-  await loadAsset("tapisCoteGauche", "dungeon/tapisCoteGauche.png");
-  await loadAsset("tapisCoteDroit", "dungeon/tapisCoteDroit.png");
-  await loadAsset("tapisAngleHautGauche", "dungeon/tapisAngleHautGauche.png");
-  await loadAsset("tapisAngleBasGauche", "dungeon/tapisAngleBasGauche.png");
-  await loadAsset("tapisAngleHautDroit", "dungeon/tapisAngleHautDroit.png");
-  await loadAsset("tapisAngleBasDroit", "dungeon/tapisAngleBasDroit.png");
-  await loadAsset("feu", "dungeon/feu.png");
-  await loadAsset("fleurs", "dungeon/fleurs.png");
-  await loadAsset("trone", "dungeon/trone.png");
-  await loadAsset("tresor01", "dungeon/tresor01.png");
-  await loadAsset("tresor02", "dungeon/tresor02.png");
-  await loadAsset("tonneau", "dungeon/tonneau.png");
-  await loadAsset("porteh", "dungeon/porteh.png");
-  await loadAsset("portev", "dungeon/portev.png");
-  await loadAsset("skeletteMort", "dungeon/skeletteMort.png");
-
-  //
-  await loadAsset("boite01", "item/Boxes/1.png");
-  await loadAsset("boite02", "item/Boxes/2.png");
-  await loadAsset("boite03", "item/Boxes/3.png");
-  await loadAsset("boite04", "item/Boxes/4.png");
-  await loadAsset("boite05", "item/Boxes/5.png");
-  await loadAsset("boite06", "item/Boxes/6.png");
-  await loadAsset("boite07", "item/Boxes/7.png");
-  await loadAsset("boite08", "item/Boxes/8.png");
-  await loadAsset("boite09", "item/Boxes/9.png");
-  await loadAsset("boite10", "item/Boxes/10.png");
-  await loadAsset("boite11", "item/Boxes/11.png");
-  await loadAsset("boite12", "item/Boxes/12.png");
-  await loadAsset("boite13", "item/Boxes/13.png");
-  await loadAsset("boite14", "item/Boxes/14.png");
-  await loadAsset("boite15", "item/Boxes/15.png");
-
-  await loadAsset("table01", "item/furniture/32x32_table.png");
-  await loadAsset("chair01", "item/furniture/32x32_chair01_blue.png");
-
-  await loadAsset("deadbg", "deadBg.webp");
-  await loadAsset("bgMenu", "bgMenu.webp");
-  await loadAsset("logodq", "logo.png");
-
-  await loadAsset("question", "question.png");
+  await Promise.all([
+    loadAsset("player", "troll.png"),
+    loadAsset("bouclier", "bouclier.png"),
+    loadAsset("chicken", "chicken.png"),
+    loadAsset("anneau", "anneau.png"),
+    loadAsset("appareil", "appareil.png"),
+    loadAsset("arbre", "arbre1.png"),
+    loadAsset("archer", "archer.png"),
+    loadAsset("armure", "armure.png"),
+    loadAsset("backpack", "backpack.png"),
+    loadAsset("bibliotheque", "bibliotheque.png"),
+    loadAsset("bouclier", "bouclier.png"),
+    loadAsset("carte-au-tresor", "carte-au-tresor.png"),
+    loadAsset("carte-au-tresor-bis", "carte-au-tresor-bis.png"),
+    loadAsset("chandelier", "chandelier.png"),
+    loadAsset("chicken", "chicken.png"),
+    loadAsset("chevre01", "chevre01.png"),
+    loadAsset("chevre02", "chevre02.png"),
+    loadAsset("cle", "cle.png"),
+    loadAsset("cleBlue", "cleBlue.png"),
+    loadAsset("cleGreen", "cleGreen.png"),
+    loadAsset("cleRed", "cleRed.png"),
+    loadAsset("collier", "collier.png"),
+    loadAsset("communsword", "communsword.png"),
+    loadAsset("cor-des-alpes", "cor-des-alpes.png"),
+    loadAsset("diamantRouge", "diamantRouge.png"),
+    loadAsset("diamantBleu", "diamantBleu.png"),
+    loadAsset("doorleft", "doorleft.png"),
+    loadAsset("doorright", "doorright.png"),
+    loadAsset("dragon", "dragon.png"),
+    loadAsset("dwarf", "dwarf.png"),
+    loadAsset("elf", "elf.png"),
+    loadAsset("fond-carte", "fond-carte.jpg"),
+    loadAsset("forgeron", "forgeron.png"),
+    loadAsset("frankenstein", "frankenstein.png"),
+    loadAsset("gateaux", "gateaux.png"),
+    loadAsset("gem", "gem.png"),
+    loadAsset("group", "group.png"),
+    loadAsset("guillotine", "guillotine.png"),
+    loadAsset("hache", "hache.png"),
+    loadAsset("king", "king.png"),
+    loadAsset("knight", "knight.png"),
+    loadAsset("la-tombe", "la-tombe.png"),
+    loadAsset("levier", "levier.png"),
+    loadAsset("magicsword", "magicsword.png"),
+    loadAsset("mission", "mission.png"),
+    loadAsset("necromancer", "necromancer.png"),
+    loadAsset("ogre01", "ogre01.png"),
+    loadAsset("ogre02", "ogre02.png"),
+    loadAsset("ogre03", "ogre03.png"),
+    loadAsset("ogre04", "ogre04.png"),
+    loadAsset("oldchandelier", "oldchandelier.png"),
+    loadAsset("panneau", "panneau.png"),
+    loadAsset("piege", "piege.png"),
+    loadAsset("player", "player.png"),
+    loadAsset("porte", "porte.png"),
+    loadAsset("potion", "potion.png"),
+    loadAsset("potion-bis", "potion-bis.png"),
+    loadAsset("potion-tr", "potion-tr.png"),
+    loadAsset("prison", "prison.png"),
+    loadAsset("prisonBlue", "prisonBlue.png"),
+    loadAsset("prisonGreen", "prisonGreen.png"),
+    loadAsset("prisonRed", "prisonRed.png"),
+    loadAsset("prisonYellow", "prisonYellow.png"),
+    loadAsset("quest", "quest.png"),
+    loadAsset("quete", "quete.png"),
+    loadAsset("raresword", "raresword.png"),
+    loadAsset("role-playing", "role-playing.png"),
+    loadAsset("serpent", "serpent.png"),
+    loadAsset("shaman", "shaman.png"),
+    loadAsset("shield", "shield.png"),
+    loadAsset("squelette", "squelette.png"),
+    loadAsset("table", "table.png"),
+    loadAsset("tasse-a-cafe", "tasse-a-cafe.png"),
+    loadAsset("tonneau", "tonneau.png"),
+    loadAsset("tonneau-de-vin", "tonneau-de-vin.png"),
+    loadAsset("troll", "troll.png"),
+    loadAsset("uniquesword", "uniquesword.png"),
+    loadAsset("village", "village.png"),
+    loadAsset("vortex", "vortex.png"),
+    loadAsset("wizard", "wizard.png"),
+    loadAsset("bg-carrelage", "bg/carrelage.png"),
+    loadAsset("bg-lava", "bg/lave.png"),
+    loadAsset("bg-mur", "bg/mur-donjon.png"),
+    loadAsset("bg-sol", "bg/sol-interieur.png"),
+    loadAsset("bg-water", "bg/eau.png"),
+    loadAsset("bg-herbe", "bg/herbe.png"),
+    loadAsset("bg-chemin", "bg/chemin.png"),
+    loadAsset("echelle", "echelle.png"),
+    loadAsset("trapp", "trapp.png"),
+    loadAsset("boat01", "boat01.png"),
+    loadAsset("boat02", "boat02.png"),
+    loadAsset("boat03", "boat03.png"),
+    loadAsset("tapisMilieu", "dungeon/tapisMilieu.png"),
+    loadAsset("tapisCoteHaut", "dungeon/tapisCoteHaut.png"),
+    loadAsset("tapisCoteBas", "dungeon/tapisCoteBas.png"),
+    loadAsset("tapisCoteGauche", "dungeon/tapisCoteGauche.png"),
+    loadAsset("tapisCoteDroit", "dungeon/tapisCoteDroit.png"),
+    loadAsset("tapisAngleHautGauche", "dungeon/tapisAngleHautGauche.png"),
+    loadAsset("tapisAngleBasGauche", "dungeon/tapisAngleBasGauche.png"),
+    loadAsset("tapisAngleHautDroit", "dungeon/tapisAngleHautDroit.png"),
+    loadAsset("tapisAngleBasDroit", "dungeon/tapisAngleBasDroit.png"),
+    loadAsset("feu", "dungeon/feu.png"),
+    loadAsset("fleurs", "dungeon/fleurs.png"),
+    loadAsset("trone", "dungeon/trone.png"),
+    loadAsset("tresor01", "dungeon/tresor01.png"),
+    loadAsset("tresor02", "dungeon/tresor02.png"),
+    loadAsset("tonneau", "dungeon/tonneau.png"),
+    loadAsset("porteh", "dungeon/porteh.png"),
+    loadAsset("portev", "dungeon/portev.png"),
+    loadAsset("skeletteMort", "dungeon/skeletteMort.png"),
+    loadAsset("boite01", "item/Boxes/1.png"),
+    loadAsset("boite02", "item/Boxes/2.png"),
+    loadAsset("boite03", "item/Boxes/3.png"),
+    loadAsset("boite04", "item/Boxes/4.png"),
+    loadAsset("boite05", "item/Boxes/5.png"),
+    loadAsset("boite06", "item/Boxes/6.png"),
+    loadAsset("boite07", "item/Boxes/7.png"),
+    loadAsset("boite08", "item/Boxes/8.png"),
+    loadAsset("boite09", "item/Boxes/9.png"),
+    loadAsset("boite10", "item/Boxes/10.png"),
+    loadAsset("boite11", "item/Boxes/11.png"),
+    loadAsset("boite12", "item/Boxes/12.png"),
+    loadAsset("boite13", "item/Boxes/13.png"),
+    loadAsset("boite14", "item/Boxes/14.png"),
+    loadAsset("boite15", "item/Boxes/15.png"),
+    loadAsset("table01", "item/furniture/32x32_table.png"),
+    loadAsset("chair01", "item/furniture/32x32_chair01_blue.png"),
+    loadAsset("deadbg", "deadBg.webp"),
+    loadAsset("bgMenu", "bgMenu.webp"),
+    loadAsset("logodq", "logo.png"),
+    loadAsset("question", "question.png"),
+  ]);
 };
 
 export const getImage = (id: string) => {
