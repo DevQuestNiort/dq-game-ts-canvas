f853c5c128f768336a3f8f993558fa34e144168e
Alexis BRIARD <briard.alexis@hotmail.fr>
2025-04-26 17:23:31 +0200

    feat: empeche le mode debug en prod

--- src/engine/GameEngine.ts
+++ src/engine/GameEngine.ts
@@ -85,7 +85,7 @@ export function newGame() {
 const bindKeys = () => {
   addEventListener("keydown", (evt) => {
     evt.preventDefault();
-    if (evt.ctrlKey && evt.shiftKey && evt.key.toLowerCase() === "a") {
+    if (import.meta.env.DEV && evt.ctrlKey && evt.shiftKey && evt.key.toLowerCase() === "a") {
       switchOption(BooleanOption.DEBUG_MODE);
       notifyViewportChanged();
     }
